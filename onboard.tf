614e662f1d4d76d751ea440fb68dea86fe6d82eb
jelinn <jelinn@gmail.com>
2020-09-22 10:11:36 -0500

    Update onboard.tf

--- onboard.tf
+++ onboard.tf
@@ -27,3 +27,20 @@ resource "tfe_team_organization_member" "test" {
   team_id = tfe_team.demoTeam.id
   organization_membership_id = tfe_organization_membership.test.id
 }
+
+resource "tfe_variable" "gcp_project" {
+  key          = "gcp_project"
+  value        = "my_value_name"
+  category     = "terraform"
+  workspace_id = $tfe_workspace.demoWorkspace.id
+  description  = "GCP Project from Instruqt"
+}
+
+resource "tfe_variable" "google_creds" {
+  key          = "GOOGLE_CREDENTIALS"
+  value        = "my_value_name"
+  category     = "env"
+  sensitive    = "true"
+  workspace_id = $tfe_workspace.demoWorkspace.id
+  description  = "GCP Credentials"
+}
